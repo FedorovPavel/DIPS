ba305bdd72de989908c7fc3222facf9726b0147a
Pavel <catkiller95@gmail.com>
2017-09-24 22:07:52 +0300

    Произведена стилизация кода под единый формат. Изменен контроллер home для маршрута '/' и запроса get Данный запрос отправляет страницу index для рендеринга у пользователя, передавая параметр Title и cars, где cars - все автомобиле в БД

--- Lab1/app/controllers/home.js
+++ Lab1/app/controllers/home.js
@@ -1,18 +1,21 @@
-var express = require('express'),
-  router = express.Router(),
-  mongoose = require('mongoose'),
-  Article = mongoose.model('Article');
+var express 	= require('express'),
+	router 		= express.Router(),
+	mongoose 	= require('mongoose'),
+	Cars 		= mongoose.model('Article');
 
 module.exports = function (app) {
-  app.use('/', router);
+	app.use('/', router);
 };
 
 router.get('/', function (req, res, next) {
-  Article.find(function (err, articles) {
-    if (err) return next(err);
-    res.render('index', {
-      title: 'Generator-Express MVC',
-      articles: articles
-    });
-  });
+	Cars.find(function (err, cars) {
+		if (err) 
+			return next(err);
+		else {
+			res.render('index', {
+				title	: 'Cars',
+				cars	: cars
+			});
+		}
+	});
 });
