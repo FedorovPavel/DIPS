51960f1063c807b4fb70753781779b5cadcba3f8
Pavel <catkiller95@gmail.com>
2017-09-24 23:29:10 +0300

    Добавлены 2 пути для контроллера 1) создает 1 машину по post запросу 2) возравщает все машины в БД по get запросу

--- Lab1/app/controllers/home.js
+++ Lab1/app/controllers/home.js
@@ -1,7 +1,7 @@
 var express 	= require('express'),
 	router 		= express.Router(),
 	mongoose 	= require('mongoose'),
-	Cars 		= mongoose.model('Article');
+	Cars 		= mongoose.model('Car');
 
 module.exports = function (app) {
 	app.use('/', router);
@@ -19,3 +19,35 @@ router.get('/', function (req, res, next) {
 		}
 	});
 });
+
+router.get('/cars', function(req, res, next) {
+	Cars.find(function(err, cars) {
+		if (err)
+			return next(err);
+		else {
+			res.status(200).send(cars);
+		}
+	});
+});
+
+router.post('/car', function(req, res, next) {
+	let n_manufacture 	= req.body.man;
+	let n_model 		= req.body.mod;
+	let n_color			= req.body.col;
+	
+	let new_car = new Cars({
+		manufacturer	: n_manufacture,
+		model 			: n_model,
+		color	  		: n_color
+	});
+	console.log(new_car);
+	new_car.save(function(err, result){ 
+		if (err)
+			return next(err);
+		else {
+			console.log(result);
+			res.status(200).send(result);
+		}
+	});
+});
+
